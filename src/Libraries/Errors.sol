e50607751f5945872d791b1d8f0b67b8362c6aa0
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-06-15 15:38:47 +0100

    Added Governance contract

---
+++ src/Libraries/Errors.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+///////////////
+/// errors ///
+/////////////
+error Governance__NotEnoughTokenBalance();
+error Governance__NotEnoughAllowance();
+error Governance__ProposalDoesNotExist();
+error Governance__ProposalInactive();
+error Governance__ProposalExpired();
+error Governance__NotEnoughVotingPower();
+error Governance__AlreadyVoted();
+error Governance__AlreadyStaked();
+error Governance__NoStakedToken();
+error Governance__OptionDoesNotExist();
